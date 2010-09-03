a5ec8e483e593251a6cf431c2415787aa81667e5
anand <anand@aagrawal.(none)>
2010-09-04 01:00:57 +0530

    anand - improving add exp UI

--- src/com/app/settleexpenses/AddExpenses.java
+++ src/com/app/settleexpenses/AddExpenses.java
@@ -1,9 +1,12 @@
 package com.app.settleexpenses;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import com.app.settleexpenses.domain.Expense;
@@ -20,6 +23,12 @@ public class AddExpenses extends Activity {
     private final Activity currentActivity = this;
     private final ContactsAdapter contacts = new ContactsAdapter(this);
 
+    protected boolean[] selections;
+    private ArrayList<String> allParticipantNames;
+    private List<Participant> allParticipants;
+
+    private TextView participantsText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +42,20 @@ public class AddExpenses extends Activity {
         Button calculateButton = (Button) findViewById(R.id.calculate);
 
         final long eventId = getIntent().getLongExtra(DbAdapter.EVENT_ID, -1);
-        final List<Participant> allParticipants = contacts.find(getIntent().getStringArrayListExtra(DbAdapter.PARTICIPANT_IDS));
-        final ArrayList<String> allParticipantNames = participantNames(allParticipants);
+        allParticipants = contacts.find(getIntent().getStringArrayListExtra(DbAdapter.PARTICIPANT_IDS));
+        allParticipantNames = participantNames(allParticipants);
 
-        final ListView participantSelectorView = (ListView) findViewById(R.id.participant_selector);
-        participantSelectorView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, allParticipantNames));
+        selections = new boolean[allParticipants.size()];
+        for (int i = 0; i< selections.length; i++ ){
+            selections[i] = true;
+        }
+
+        participantsText = (TextView) findViewById(R.id.participants);
+        updateParticipantList();
+
+
+        Button editParticipantList = (Button) findViewById(R.id.edit_participants);
+        editParticipantList.setOnClickListener(new ButtonClickHandler());
 
         final Spinner paidBy = (Spinner) findViewById(R.id.paid_by);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, allParticipantNames);
@@ -47,7 +65,7 @@ public class AddExpenses extends Activity {
         confirmButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                if(isInValid(expenseTitleText.getText().toString()) || isInValid(expenseAmount.getText().toString())){
+                if (isInValid(expenseTitleText.getText().toString()) || isInValid(expenseAmount.getText().toString())) {
                     Toast toast = Toast.makeText(currentActivity, "Please complete the required fields", Toast.LENGTH_LONG);
                     toast.show();
                     return;
@@ -57,7 +75,7 @@ public class AddExpenses extends Activity {
                 dbAdapter.open();
 
                 float amount = Float.parseFloat(expenseAmount.getText().toString());
-                ArrayList<Participant> participants = selectedParticipants(participantSelectorView, allParticipants);
+                ArrayList<Participant> participants = selectedParticipants();
 
                 Expense expense = new Expense(expenseTitleText.getText().toString(), amount,
                         eventId, allParticipants.get(paidBy.getSelectedItemPosition()), participants);
@@ -82,7 +100,7 @@ public class AddExpenses extends Activity {
         });
     }
 
-    private boolean isInValid(String value){
+    private boolean isInValid(String value) {
         return value != null && value.trim().length() == 0;
     }
 
@@ -94,14 +112,48 @@ public class AddExpenses extends Activity {
         return result;
     }
 
-    private ArrayList<Participant> selectedParticipants(ListView participantSelectorView, List<Participant> allParticipants) {
+    private ArrayList<Participant> selectedParticipants() {
         ArrayList<Participant> participants = new ArrayList<Participant>();
-        SparseBooleanArray checkedItemPositions = participantSelectorView.getCheckedItemPositions();
-        for (int i = 0; i < checkedItemPositions.size(); i++) {
-            if (checkedItemPositions.valueAt(i)) {
+        for (int i = 0; i < selections.length; i++) {
+            if (selections[i]) {
                 participants.add(allParticipants.get(i));
             }
         }
         return participants;
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Log.d("addExpense", allParticipantNames.toString());
+        String[] participants = allParticipantNames.toArray(new String[allParticipantNames.size()]);
+        return new AlertDialog.Builder(this)
+                .setTitle("Planets")
+                .setMultiChoiceItems(participants, selections, new DialogInterface.OnMultiChoiceClickListener(){
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                    }
+                })
+                .setPositiveButton("OK", new DialogButtonClickHandler())
+                .create();
+    }
+
+    private void updateParticipantList() {
+        String selectedParticipants = participantNames(selectedParticipants()).toString();
+        participantsText.setText(selectedParticipants.substring(1, selectedParticipants.length() - 1));
+    }
+
+    public class DialogButtonClickHandler implements DialogInterface.OnClickListener {
+        public void onClick(DialogInterface dialog, int clicked) {
+            switch (clicked) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    updateParticipantList();
+                    break;
+            }
+        }
+    }
+
+    public class ButtonClickHandler implements View.OnClickListener {
+        public void onClick(View view) {
+            showDialog(0);
+        }
     }
 }
