c9c2df10ee6965a361940e5d885f22a2f4a03452
anand <anand@aagrawal.(none)>
2010-10-14 01:31:30 +0530

    adding first robotium test

---
+++ SettleExpensesFunctionalTest/src/com/app/settleexpenses/test/functional/EndToEndTest.java
@@ -0,0 +1,37 @@
+package com.app.settleexpenses.test.functional;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
+
+import com.app.settleexpenses.ShowEvents;
+import com.jayway.android.robotium.solo.Solo;
+
+public class EndToEndTest extends ActivityInstrumentationTestCase2<ShowEvents>{
+	
+	private Solo solo;
+	
+	public EndToEndTest() {
+		super("com.app.settleexpenses", ShowEvents.class);
+	}
+	
+	public void setUp() {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	@Smoke
+	public void testX() {
+		solo.clickOnText("Add an event");
+	}
+	
+	public void tearDown() throws Exception{
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();	
+	}
+
+}
